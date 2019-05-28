91ac01987f1bdcff82c0dcff3a35d394fab03895
Johan Andrey Bosso <bossojohan@gmail.com>
2019-05-28 15:06:56 +0200

    first checkpoint

--- Pose_Generator.py
+++ Pose_Generator.py
@@ -18,54 +18,82 @@ elif MODE is "MPI" :
     nPoints = 15
     POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
     
-frame = cv2.imread("trono.jpg")
-frameCopy = np.copy(frame)
-frameWidth = frame.shape[1]
-frameHeight = frame.shape[0]
+#frame = cv2.imread("karate.jpg")
+#frameCopy = np.copy(frame)
+#frameWidth = frame.shape[1]
+#frameHeight = frame.shape[0]
+#frameHeight = frame.shape[0]
+#
+
+
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
+inWidth = 368
+inHeight = 368
 threshold = 0.1
 
-inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
-                          (0, 0, 0), swapRB=False, crop=False)
+input_source = "io.mp4"
+cap = cv2.VideoCapture(input_source)
+hasFrame, frame = cap.read()
 
-net.setInput(inpBlob)
+vid_writer = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
 
-output = net.forward()
-H = output.shape[2]
-W = output.shape[3]
+while cv2.waitKey(1) < 0:
+    t = time.time()
+    hasFrame, frame = cap.read()
+    frameCopy = np.copy(frame)
+    if not hasFrame:
+        cv2.waitKey()
+        break
 
-# Empty list to store the detected keypoints
-points = []
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
 
-for i in range(nPoints):
-    # confidence map of corresponding body's part.
-    probMap = output[0, i, :, :]
+    inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
+                              (0, 0, 0), swapRB=False, crop=False)
+    net.setInput(inpBlob)
+    output = net.forward()
 
-    # Find global maxima of the probMap.
-    minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-    
-    # Scale the point to fit on the original image
-    x = (frameWidth * point[0]) / W
-    y = (frameHeight * point[1]) / H
+    H = output.shape[2]
+    W = output.shape[3]
+    # Empty list to store the detected keypoints
+    points = []
+    blank_image = np.zeros(frame.shape, np.uint8)
+    for i in range(nPoints):
+        # confidence map of corresponding body's part.
+        probMap = output[0, i, :, :]
 
-    if prob > threshold : 
-        cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-        cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
-        cv2.circle(frame, (int(x), int(y)), 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+        # Find global maxima of the probMap.
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+        
+        # Scale the point to fit on the original image
+        x = (frameWidth * point[0]) / W
+        y = (frameHeight * point[1]) / H
 
-        # Add the point to the list if the probability is greater than the threshold
-        points.append((int(x), int(y)))
-    else :
-        points.append(None)
+        if prob > threshold : 
+            cv2.circle(blank_image, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+            cv2.putText(blank_image, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+
+            # Add the point to the list if the probability is greater than the threshold
+            points.append((int(x), int(y)))
+        else :
+            points.append(None)
 
 # Draw Skeleton
-for pair in POSE_PAIRS:
-    partA = pair[0]
-    partB = pair[1]
 
-    if points[partA] and points[partB]:
-        cv2.line(frame, points[partA], points[partB], (0, 255, 255), 3)
-print(points)
-plt.figure(figsize=[10,10])
-plt.imshow(cv2.cvtColor(frameCopy, cv2.COLOR_BGR2RGB))
-plt.figure(figsize=[10,10])
-plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    for pair in POSE_PAIRS:
+        partA = pair[0]
+        partB = pair[1]
+
+        if points[partA] and points[partB]:
+            cv2.line(blank_image, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
+            print(points)
+    cv2.putText(blank_image, "time taken = {:.2f} sec".format(time.time() - t), (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+    # cv2.putText(frame, "OpenPose using OpenCV", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+    # cv2.imshow('Output-Keypoints', frameCopy)
+    cv2.imshow('Output-Skeleton', blank_image)
+
+    vid_writer.write(blank_image)
+
+vid_writer.release()
